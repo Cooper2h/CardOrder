85a0695a6f4421122028f45ae86769c9ffb9f1a7
Cooper2h <step.stepstep@yandex.ru>
2024-12-10 15:39:35 +0300

    fixed selectors

--- src/test/java/ru/netology/CardOrderNegativeTest.java
+++ src/test/java/ru/netology/CardOrderNegativeTest.java
@@ -16,80 +16,79 @@ public class CardOrderNegativeTest {
 
     @Test
     public void shouldShowErrorInvalidNameFieldEmpty() {
-        $("[data-test-id=name] input").setValue(""); // Поле имени пустое
+        $("[data-test-id=name] input").setValue("");
         $("[data-test-id=phone] input").setValue("+79862146556");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
 
-        // Ищем ошибку по конкретному полю "Имя"
-        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     public void shouldShowErrorInvalidNameField() {
-        $("[data-test-id=name] input").setValue("Anatoly Wasserman"); // Некорректное имя
+        $("[data-test-id=name] input").setValue("Anatoly Wasserman");
         $("[data-test-id=phone] input").setValue("+79862146556");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
 
-        // Проверяем сообщение об ошибке для поля "Имя"
-        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаны неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Имя и Фамилия указаны неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     public void shouldShowErrorInvalidNumberTelephoneField() {
         $("[data-test-id=name] input").setValue("Анатолий Вассерман");
-        $("[data-test-id=phone] input").setValue("+798621465565"); // Некорректный номер телефона
+        $("[data-test-id=phone] input").setValue("+798621465565");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
 
-        // Проверяем сообщение об ошибке для телефона
-        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     public void shouldShowErrorInvalidNumberTelephoneFieldWithWord() {
         $("[data-test-id=name] input").setValue("Анатолий Вассерман");
-        $("[data-test-id=phone] input").setValue("Wasserman"); // Текст вместо номера
+        $("[data-test-id=phone] input").setValue("Wasserman");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
 
-        // Проверяем сообщение об ошибке для телефона
-        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     public void shouldShowErrorInvalidNumberTelephoneFieldEmpty() {
         $("[data-test-id=name] input").setValue("Анатолий Вассерман");
-        $("[data-test-id=phone] input").setValue(""); // Поле телефона пустое
+        $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
 
-        // Проверяем сообщение об ошибке для телефона
-        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     public void shouldShowErrorInvalidTermsAgreement() {
         $("[data-test-id=name] input").setValue("Анатолий Вассерман");
         $("[data-test-id=phone] input").setValue("+79862146556");
-        $("[type=button]").click(); // Не ставим галочку "Согласие"
+        $("[type=button]").click();
 
-        // Проверяем сообщение об ошибке для чекбокса
-        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $("[data-test-id=agreement].input_invalid .checkbox__text")
+                .shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
     @Test
     public void shouldShowErrorInvalidValueFieldsEmpty() {
-        $("[data-test-id=name] input").setValue(""); // Оба поля пустые
+        $("[data-test-id=name] input").setValue("");
         $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
 
-        // Проверяем сообщение об ошибке для имени
-        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
-
-        // Проверяем сообщение об ошибке для телефона
-        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 }
